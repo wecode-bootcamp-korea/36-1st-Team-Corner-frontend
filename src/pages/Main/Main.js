b7c3424db2fafa50118fb0c4d4437b260e8c8e8a
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-17 14:01:37 +0900

    Add: 메인 html 및 컴포넌트화 초안

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import './Main.scss';
 
+const Card = () => {
+  return (
+    <div className="card">
+      <img src="url" alt="cardImage" />
+      <h3 className=" productName">hello</h3>
+      <span className="currentPrice" />
+      <span className="price" />
+      <p className="reviewCount">
+        리뷰 <span className="reviewCounter">0</span>
+      </p>
+    </div>
+  );
+};
+
+const ItemList = () => {
+  return (
+    <div className="itemList">
+      <Card />
+    </div>
+  );
+};
+
+const Pagination = () => {
+  return (
+    <div className="pagination">
+      <ItemList />
+      <button>이전</button>
+      <span>1 2 3</span>
+      <button>다음</button>
+    </div>
+  );
+};
+
 const Main = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <img className="carousel" src="" alt="carouselImage" />
+      <div className="productsInMain">
+        <Pagination />
+      </div>
+    </>
+  );
 };
 
 export default Main;
