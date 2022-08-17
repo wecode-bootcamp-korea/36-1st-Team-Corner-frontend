a67d6684bc52587d1c0aed9cb704edbb282759f1
minjae46 <minjaejo46@gmail.com>
2022-08-17 15:55:06 +0900

    Add: index.html에 fontawesome script 추가

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <div className="navCarousel">
+        <span>안녕하세요 이것은</span>
+        <span>매우매우 멋진</span>
+        <span>캐러셀 입니다</span>
+      </div>
+      <div className="navArea">
+        <h1 className="logo">BODYLIKE</h1>
+        <div className="navMenu">
+          <ul className="navMenuList">
+            <li>
+              <Link className="link" to="/login">
+                로그인
+              </Link>
+            </li>
+            <li>
+              <Link className="link" to="/cart">
+                장바구니
+              </Link>
+              <div className="countBg">0</div>
+            </li>
+            <li>
+              <i class="fas fa-search" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
