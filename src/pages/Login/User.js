497fe4d58dac7726109b8d6f9eb6071dc2aac53e
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-08-18 16:43:15 +0900

    fetch 추가

--- src/pages/Login/User.js
+++ src/pages/Login/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const User = ({ content }) => {
   const { title, btnText, img } = content;
@@ -8,26 +8,73 @@ const User = ({ content }) => {
     userId: '',
     userPw: '',
   });
-  // const [emailInfo, setEmailInfo] = useState('')
+
   const onChangeinfo = e => {
     const { name, value } = e.target;
     setInfo({ ...info, [name]: value });
   };
 
-  const navigate = useNavigate();
-  const goToLogin = e => {
-    navigate('/login');
-    alert('회원가입이 완료 되었습니다. 로그인해주세요');
-  };
+  // const navigate = useNavigate();
+  // const goToLogin = e => {
+  //   navigate('/login');
+  //   alert('회원가입이 완료 되었습니다. 로그인해주세요');
+  // };
 
   const passed =
-    // info.userId.toLowerCase() &&
     info.userId.length > 5 &&
     info.userPw.length > 5 &&
     info.userId.includes('@');
-  // 유효성 검사 기준 (아이디 소문자,(length 4~16), 비밀번호 대소문자 포함 )
+  // 유효성 검사 기준 (아이디(이메일) 소문자,(length 4~16), 비밀번호 대소문자 포함 )
 
-  console.log(passed);
+  const toSignUp = e => {
+    e.preventDefault();
+    fetch('http://10.58.0.71:3000/users/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: info.userId,
+        user_name: 'Adam',
+        password: info.userPw.toString(),
+        email: info.userId,
+        phone_number: '010-2421-1241',
+        birth_date: '1994-04-04',
+      }),
+    });
+    // .then(response => response.json())
+    // .then(data => {
+    //   if (data.message === 'SUCCESS') {
+    //     navigate('/login');
+    //   }
+    // });
+  };
+
+  const toLogin = e => {
+    e.preventDefault();
+    fetch('http://10.58.0.71:3000/post/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: info.userId,
+        user_name: 'Adam',
+        password: info.userPw.toString(),
+        email: info.userId,
+        phone_number: '010-2421-1241',
+        birth_date: '1994-04-04',
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.MESSAGE === 'login success') {
+          localStorage.setItem('token', data.TOKEN);
+        } else {
+          alert('확인해주세요');
+        }
+      });
+  };
 
   return (
     <div className="layout">
@@ -52,7 +99,7 @@ const User = ({ content }) => {
               onChange={onChangeinfo}
             />
 
-            <button disabled={!passed} onClick={goToLogin}>
+            <button disabled={!passed} onClick={toLogin}>
               {btnText}
             </button>
 
