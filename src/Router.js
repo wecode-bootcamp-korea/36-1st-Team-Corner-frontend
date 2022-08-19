486f07ebea06d31c285af3fb89bb17bb3413766c
minjae46 <minjaejo46@gmail.com>
2022-08-19 20:46:17 +0900

    Search.js 파일 생성

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import Main from './pages/Main/Main';
+import Search from './pages/Search/Search';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/product/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Search/Search.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Search.scss';
+
+const Search = () => {
+  return <div className="search">검색된 상품 리스트입니다</div>;
+};
+
+export default Search;
