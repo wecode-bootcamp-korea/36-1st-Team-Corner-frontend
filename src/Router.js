6b91eb81de00a3a7d60405d0abb4a074860c5af4
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-08-20 13:12:59 +0900

    브런치 생성

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import Product from './pages/Product/Product';
 
 const Router = () => {
   return (
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
