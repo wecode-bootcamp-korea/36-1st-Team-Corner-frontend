93a6aa9ce3cbe0e1184b6a14adb6758431564730
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-08-20 20:08:39 +0900

    fetch 함수 수정

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -10,7 +10,7 @@ const Product = () => {
   const navigate = useNavigate;
 
   useEffect(() => {
-    fetch(`https://reqres.in/api/product/${id}`)
+    fetch(`https://reqres.in/api/product/${productId}`)
       .then(response => response.json())
       .then(result => setProduct(result.data));
   }, [productId]);
@@ -20,7 +20,7 @@ const Product = () => {
     const token = localStorage.getItem('token') || '';
 
     if (token) {
-      fetch(`http://10.58.4.114:8000/user/cart/${product_id}`, {
+      fetch(`http://10.58.4.114:8000/user/cart/${productId}`, {
         method: 'POST',
         headers: {
           AUTHORIZATION: token,
@@ -39,37 +39,63 @@ const Product = () => {
           }
         });
     }
+  };
 
-    return (
-      <div className="product">
-        <div className="productMargin">
-          <div className="productPage">
-            <img className="productImg" alt="productImg" src={img} />
-            <form className="productForm">
-              <div className="priceTitle"> {title} </div>
-              <div className="price">
-                <div className="priceTag"> {price} 원 </div>
-                <div className="salePrice"> ({price} * 0.8) 원 </div>
-              </div>
-              <div className="totalPrice">
-                <span className="totalPriceText"> 총 상품 금액 </span>
-                <span className="totalPriceNum"> {price} 원 </span>
-              </div>
+  const goToCart = e => {
+    e.preventDefault();
+    const token = localStorage.getItem('token') || '';
 
-              <div className="btn">
-                <button className="buyBtn" onClick={toBuy}>
-                  바로 구매하기
-                </button>
-                <button className="cart" onClick={toBuy}>
-                  장바구니
-                </button>
-              </div>
-            </form>
-          </div>
+    if (token) {
+      fetch(`http://10.58.4.114:8000/user/cart/${productId}`, {
+        method: 'POST',
+        headers: {
+          AUTHORIZATION: token,
+        },
+        body: JSON.stringify({
+          quantity: 1,
+        }),
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (res.ok === true) {
+            return navigate('/cart');
+          } else {
+            alert('로그인이 필요한 기능입니다');
+            navigate('/cart');
+          }
+        });
+    }
+  };
+
+  return (
+    <div className="product">
+      <div className="productMargin">
+        <div className="productPage">
+          <img className="productImg" alt="productImg" src={img} />
+          <form className="productForm">
+            <div className="priceTitle"> {title} </div>
+            <div className="price">
+              <div className="priceTag"> {price} 원 </div>
+              <div className="salePrice"> ({price} * 0.8) 원 </div>
+            </div>
+            <div className="totalPrice">
+              <span className="totalPriceText"> 총 상품 금액 </span>
+              <span className="totalPriceNum"> {price} 원 </span>
+            </div>
+
+            <div className="btn">
+              <button className="buyBtn" onClick={toBuy}>
+                바로 구매하기
+              </button>
+              <button className="cart" onClick={goToCart}>
+                장바구니
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
 export default Product;
