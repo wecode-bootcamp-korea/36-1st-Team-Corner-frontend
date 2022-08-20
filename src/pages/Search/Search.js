8cb1679d8258cf45e8879d5d5036c65bd22b7135
minjae46 <minjaejo46@gmail.com>
2022-08-20 18:02:26 +0900

    Add: 검색어 서버에 보내서 검색된 상품 리스트만 가져오는 기능 구현

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import './Search.scss';
 
 const Search = () => {
+  const [searchedProduct, setSearchedProduct] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-
   const searchedWord = searchParams.get('q');
 
+  useEffect(() => {
+    fetch(`http://10.58.7.174:3000/products/search?q=${searchedWord}`, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(result => setSearchedProduct(result.data));
+  }, [searchedWord]);
+
   return (
-    <div className="search">
-      <h1>검색</h1>
-      <span>쿼리 스트링 : {queryString}</span>
-    </div>
+    <>
+      <div className="search">
+        <h1>검색</h1>
+        <span>쿼리 스트링 : {searchedWord}</span>
+      </div>
+      <ul className="searchedProductList">
+        {searchedProduct.map((product, i) => {
+          return <li key={product.id}>상품명:{product.name}</li>;
+        })}
+      </ul>
+    </>
   );
 };
 
