2ccc8bc745b284832714f32cbcc5ea07447f6e9b
minjae46 <minjaejo46@gmail.com>
2022-08-20 13:37:12 +0900

    차후 받아와야할 API에 대한 주석 추가

--- src/pages/ProductsList/ProductsList.js
+++ src/pages/ProductsList/ProductsList.js
@@ -8,18 +8,21 @@ const ProductsList = () => {
   const [products, setProducts] = useState();
 
   useEffect(() => {
-    fetch(`API주소/products/list/${categoryId}`)
+    fetch(`API주소/products/list/${categoryId}`, {
+      method: 'GET',
+    })
+      //TODO : 차후 서버 주소로 변경 필요
       .then(response => response.json())
       .then(result => setProducts(result));
   }, [categoryId]);
 
-  const { id, name } = products;
+  const { name } = products;
 
   return (
     <div className="productsList">
-      <p>상품 id:{id}</p>
       <p>상품 name:{name}</p>
     </div>
+    //TODO : merge 후 pagination page 컴포넌트 추가 필요
   );
 };
 
