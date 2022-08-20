d6ecd84ee4fde4b430de876020939249efd9db7e
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-20 16:27:27 +0900

    add: 모달 기능 구현 리팩토링 아직 안함

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from './Modal';
 import './Main.scss';
 
 const Main = () => {
-  return <h1>Hello World</h1>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const eventHandler = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <>
+      <button onClick={eventHandler}>click me</button>
+      <Modal isOpen={isModalOpen} toggleModal={eventHandler} />
+    </>
+  );
 };
 
 export default Main;
---
+++ src/pages/Main/Modal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './modal.scss';
+
+const Modal = ({ isOpen, toggleModal }) => {
+  return (
+    <div className="modal" id={isOpen ? 'openModal' : 'closeModal'}>
+      <div className="header">
+        <h3 className="headerTitle"> 장바구니 담기</h3>
+      </div>
+      <div className="content">
+        <img className="cart" src="/images/cart-icon.svg" alt="카트이미지" />
+        <p className="textContent">장바구니에 상품이 정상적으로 담겼습니다.</p>
+      </div>
+      <div className="modalButtons">
+        <Link to="/">
+          <button className="goToCartButton">장바구니 이동</button>
+        </Link>
+        <button className="backToShopButton" onClick={toggleModal}>
+          쇼핑 계속하기
+        </button>
+      </div>
+      <div className="closeButton" onClick={toggleModal}>
+        {'\u2715'}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
