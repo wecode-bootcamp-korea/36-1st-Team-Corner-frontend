620d0e880e7bb791bb7c9a472c0a491c0dc9af95
minjae46 <minjaejo46@gmail.com>
2022-08-21 22:08:52 +0900

    Add: Footer 컴포넌트 분리 및 상수 데이터화

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
+import Footer from './components/Footer/Footer';
 
 const Router = () => {
   return (
@@ -10,6 +11,7 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/main" element={<Main />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
