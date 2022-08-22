853bb245f8e941e2e464c38c72f354d741fc4b20
minjae46 <minjaejo46@gmail.com>
2022-08-22 11:31:40 +0900

    Modify: 상단 캐러셀 수정

--- src/components/Nav/NavCarousel.js
+++ src/components/Nav/NavCarousel.js
@@ -6,18 +6,3 @@ const NavCarousel = () => {
 };
 
 export default NavCarousel;
-
-// const CAROUSEL_CONTENTS = [
-//   {
-//     id: 1,
-//     text: '이것은',
-//   },
-//   {
-//     id: 2,
-//     text: '매우매우 멋진',
-//   },
-//   {
-//     id: 3,
-//     text: '캐러셀 입니다',
-//   },
-// ];
