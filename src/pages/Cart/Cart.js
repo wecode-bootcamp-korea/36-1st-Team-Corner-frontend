b00e8dc954d7da828a77b8a72a661f2e245b60f8
minjae46 <minjaejo46@gmail.com>
2022-08-23 15:29:56 +0900

    Add : 장바구니 비었을 때 알림  기능 추가

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -5,12 +5,12 @@ import './Cart.scss';
 
 const Cart = () => {
   const [cartProducts, setCartProducts] = useState([]);
-  const [checkedProducts, setCheckedProducts] = useState([]);
+  console.log(cartProducts.length);
 
   //const token = localStorage.getItem('token');
 
   const token =
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3LCJleHAiOjE2NjEyMDQyODUsImlhdCI6MTY2MTE2ODI4NX0.-9_PKB_y2MLusZijYYi95Ch1mFzU8of_tdSwVTx6V7k';
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjM3LCJleHAiOjE2NjEyNTI4MjYsImlhdCI6MTY2MTIxNjgyNn0.D28GQdpcZ8MTOAXRu_T3vRxT9sGqB59hFnk9Xo0PFIY';
 
   useEffect(() => {
     fetch('http://10.58.0.117:3000/cart/product', {
@@ -24,34 +24,6 @@ const Cart = () => {
       .then(result => setCartProducts(result.data));
   }, []);
 
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/data/cartMockData.json', {
-  //     method: 'GET',
-  //   })
-  //     .then(response => response.json())
-  //     .then(result => setCartProducts(result.data));
-  // }, []);
-
-  const isAllProductsChecked =
-    cartProducts.length !== 0 && cartProducts.length === checkedProducts.length;
-
-  const handleCheckAll = () => {
-    setCheckedProducts(cartProducts.map(product => product.id));
-    if (isAllProductsChecked) {
-      setCheckedProducts([]);
-    }
-  };
-
-  const handleCheck = e => {
-    const { id, checked } = e.target;
-    setCheckedProducts([...checkedProducts, Number(id)]);
-    if (!checked) {
-      setCheckedProducts(
-        checkedProducts.filter(product => product !== Number(id))
-      );
-    }
-  };
-
   const deleteAllProducts = () => {
     if (window.confirm('장바구니를 비우시겠습니까?')) {
       fetch('http://10.58.0.117:3000/cart/product', {
@@ -63,23 +35,10 @@ const Cart = () => {
       }).then(response => {
         if (response.status === 204) {
           setCartProducts([]);
-          setCheckedProducts('');
         }
       });
     }
   };
-
-  // const deleteProduct = id = {
-  //   fetch(`http://10.58.0.117:3000/cart/product/${id}`, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: token,
-  //     },
-  //   })
-  //   .then(response => response.json())
-  //   .then(result => setCartProducts(result.data))
-  // };
 
   const deleteThisProduct = id => {
     if (window.confirm('선택하신 상품을 삭제하시겠습니까?')) {
@@ -97,123 +56,101 @@ const Cart = () => {
     }
   };
 
-  const deleteSelectedProduct = () => {
-    if (window.confirm('선택하신 상품을 삭제하시겠습니까?')) {
-      fetch(`http://10.58.0.117:3000/cart/product`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-        },
-        body: JSON.stringify({
-          id: checkedProducts,
-        }),
-      }).then(response => response.json());
-    }
-  };
-
   return (
     <div className="cart">
       <div className="cartContents">
         <h1 className="cartTitle">장바구니</h1>
-        <table className="cartProductArea">
-          <thead>
-            <tr>
-              <th>
-                <input
-                  type="checkbox"
-                  checked={isAllProductsChecked}
-                  onClick={handleCheckAll}
-                />
-              </th>
-              <th>이미지</th>
-              <th>상품명</th>
-              <th>판매가</th>
-              <th>수량</th>
-              <th>합계</th>
-              <th>선택</th>
-            </tr>
-          </thead>
-          <tbody className="cartProductBox">
-            {cartProducts.map(product => {
-              const { id, name, price, quantity, thumbnail_image_url } =
-                product;
-              checkedProducts.includes(id);
-              return (
-                <tr key={id}>
+        {cartProducts.length === 0 ? (
+          <div className="cartEmpty">
+            <h3>장바구니가 비어 있습니다.</h3>
+          </div>
+        ) : (
+          <div className="cartFull">
+            <table className="cartProductArea">
+              <thead>
+                <tr>
+                  <th>이미지</th>
+                  <th>상품명</th>
+                  <th>판매가</th>
+                  <th>수량</th>
+                  <th>합계</th>
+                  <th>선택</th>
+                </tr>
+              </thead>
+              <tbody className="cartProductBox">
+                {cartProducts.map(product => {
+                  const { id, name, price, quantity, thumbnail_image_url } =
+                    product;
+                  return (
+                    <tr key={id}>
+                      <td className="tdImgbox">
+                        <div className="cartProductImg">
+                          <img alt="상품" src={thumbnail_image_url} />
+                        </div>
+                      </td>
+                      <td className="tdNamebox">
+                        <span>{name}</span>
+                      </td>
+                      <td className="tdPricebox">
+                        <span>{price}</span>
+                      </td>
+                      <td className="tdCountbox">
+                        <div className="btnbox">
+                          <div className="countBtnbox">
+                            <button>-</button>
+                            <span>{quantity}</span>
+                            <button>+</button>
+                          </div>
+                          <button>수량 변경</button>
+                        </div>
+                      </td>
+                      <td className="tdTotalPricebox">
+                        <span>{price}</span>
+                      </td>
+                      <td className="tdBtnsbox">
+                        <button>주문하기</button>
+                        <button
+                          className="deleteThisBtn"
+                          onClick={() => {
+                            deleteThisProduct(id);
+                          }}
+                        >
+                          삭제하기
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+
+            <div className="deleteBtnBox">
+              <button className="deleteAllBtn" onClick={deleteAllProducts}>
+                장바구니비우기
+              </button>
+            </div>
+            <table className="cartTotalPrice">
+              <thead>
+                <tr>
+                  <th>총 상품금액</th>
+                  <th>결제예정금액</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
                   <td>
-                    <input
-                      key={id}
-                      type="checkbox"
-                      checked={checkedProducts.includes(id)}
-                      id={id}
-                      onClick={handleCheck}
-                    />
+                    <span>{2000}</span>원
                   </td>
                   <td>
-                    <div className="cartProductImg">
-                      <img alt="상품" src={thumbnail_image_url} />
-                    </div>
-                  </td>
-                  <td>
-                    <span>{name}</span>
-                  </td>
-                  <td>
-                    <span>{price}</span>
-                  </td>
-                  <td>
-                    <button>-</button>
-                    <div>{quantity}</div>
-                    <button>+</button>
-                  </td>
-                  <td>
-                    <span>{price}</span>
-                  </td>
-                  <td>
-                    <button>주문하기</button>
-                    <button
-                      className="deleteThisBtn"
-                      onClick={() => {
-                        deleteThisProduct(id);
-                      }}
-                    >
-                      삭제하기
-                    </button>
+                    <span>{2000}</span>원
                   </td>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div className="deleteBtnBox">
-          <button className="deleteSelectedBtn" onclick={deleteSelectedProduct}>
-            선택상품삭제
-          </button>
-          <button className="deleteAllBtn" onClick={deleteAllProducts}>
-            장바구니비우기
-          </button>
-        </div>
-        <table className="cartTotalPrice">
-          <thead>
-            <tr>
-              <th>총 상품금액</th>
-              <th>결제예정금액</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <span>{2000}</span>원
-              </td>
-              <td>
-                <span>{2000}</span>원
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </tbody>
+            </table>
+          </div>
+        )}
         <div className="bottomBtnBox">
           <button className="bottomBtn">전체상품주문</button>
-          <button className="bottomBtn">선택상품주문</button>
           <button className="bottomBtn">쇼핑계속하기</button>
         </div>
         <CartFooter />
