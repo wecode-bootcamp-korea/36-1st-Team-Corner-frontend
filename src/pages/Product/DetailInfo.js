9aa260dc022b7044a307eeaab9c0861105538882
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-08-23 17:25:37 +0900

    detailInfo.js 생성

--- src/pages/Product/DetailInfo.js
+++ src/pages/Product/DetailInfo.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './DetailInfo.scss';
 
 const DetailInfo = () => {
-  return <div>DetailInfo</div>;
+  return (
+    <div className="detailInfo">
+      <div classname="paymentInformation">상품결제정보</div>
+      <div classname="delivery">배송정보</div>
+      <div classname="ExchangeAndReturn ">교환 및 반품정보</div>
+    </div>
+  );
 };
 
 export default DetailInfo;
