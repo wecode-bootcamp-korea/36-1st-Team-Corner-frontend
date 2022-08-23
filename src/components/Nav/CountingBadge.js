dc77470d55c47a521765d4c43adbd250bd6aa6b0
minjae46 <minjaejo46@gmail.com>
2022-08-23 16:58:58 +0900

    장바구니 카운팅 뱃지 통신 성공

--- src/components/Nav/CountingBadge.js
+++ src/components/Nav/CountingBadge.js
@@ -4,15 +4,23 @@ import './CountingBadge.scss';
 const CountingBadge = () => {
   const [productCounting, setProductCounting] = useState('');
 
+  //const token = localStorage.getItem('token');
+  const token =
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjYsImV4cCI6MTY2MTQ0MzI2NSwiaWF0IjoxNjYxMDgzMjY1fQ.KmF-Jp46fdHKwxS01SJ8PtF5yD1SkQP8rwQFA6tU9rQ';
+
   useEffect(() => {
-    fetch('http://10.58.0.117:3001/cart/counting', {
+    fetch('http://10.58.2.193:3000/cart/counting', {
       method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
     })
       .then(response => response.json())
-      .then(result => console.log(result.data));
+      .then(result => setProductCounting(result.data));
   }, [productCounting]);
 
-  return <div className="countingBadge">{1}</div>;
+  return <div className="countingBadge">{productCounting}</div>;
 };
 
 export default CountingBadge;
