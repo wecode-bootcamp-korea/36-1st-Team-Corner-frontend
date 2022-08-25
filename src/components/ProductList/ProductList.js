7ee8ea599f1abd6aef15cbcdde95ceb0b6ed55ad
minjae46 <minjaejo46@gmail.com>
2022-08-25 18:17:54 +0900

    Add : 상품 상세 페이지로 이동하는 경로 추가

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ProductCard from '../ProductCard/ProductCard';
+import { Link } from 'react-router-dom';
 import './ProductList.scss';
 
 const ProductList = ({ products }) => {
   return (
     <div className="productList">
       {products.map(product => {
-        return <ProductCard key={product.id} product={product} />;
+        return (
+          <Link key={product.id} to={`/product/${product.id}`}>
+            <ProductCard key={product.id} product={product} />;
+          </Link>
+        );
       })}
     </div>
   );
