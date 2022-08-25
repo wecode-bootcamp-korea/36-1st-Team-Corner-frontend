31e34baf0352810cfd2e10ed84964b906ef88e27
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-25 18:11:32 +0900

    ADD: 메인 merge  conflictc 추가  해결2

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from 
 import Nav from './components/Nav/Nav';
 import Login from './pages/Login/Login';
 import Cart from './pages/Cart/Cart';
@@ -9,7 +8,6 @@ import Product from './pages/Product/Product';
 import Main from './pages/Main/Main';
 import Search from './pages/Search/Search';
 
-
 import Footer from './components/Footer/Footer';
 
 const Router = () => {
@@ -17,13 +15,11 @@ const Router = () => {
     <BrowserRouter>
       <Nav />
       <Routes>
-      
         <Route path="/" element={<Main />} />;
         <Route path="/carts/product/:id" element={<Product />} />;
         <Route path="/auth/signIn" element={<Login />} />
         <Route path="/auth/signUp" element={<Login />} />
         <Route path="/carts" element={<Cart />} />
-
         <Route path="/product/list" element={<CategoryProduct />} />
         <Route path="/product/search" element={<Search />} />
       </Routes>
