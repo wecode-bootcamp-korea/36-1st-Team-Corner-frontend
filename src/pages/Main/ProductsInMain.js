8a3ebb64a7be07fc4bcd70a0b4851075fe054c71
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-25 18:13:54 +0900

    ADD: 메인 merge  url  추가

--- src/pages/Main/ProductsInMain.js
+++ src/pages/Main/ProductsInMain.js
@@ -6,9 +6,12 @@ const ProductsInMain = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://10.58.0.117:3000/', {
-      method: 'GET',
-    })
+    fetch(
+      'http://10.58.0.117:3000/product/list?page=1&pageSize=9&orderBy=price',
+      {
+        method: 'GET',
+      }
+    )
       .then(res => res.json())
       .then(data => setProducts(data.data));
   }, []);
