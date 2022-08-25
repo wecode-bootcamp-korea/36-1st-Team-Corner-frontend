284b5dd1cd1777872a4908839e6c0d7048757da1
Qrio <kyuchoi@YUNMIui-MacBookAir.local>
2022-08-25 17:41:05 +0900

    ADD: 메인 merge  conflict 해결

--- src/pages/Main/ProductsInMain.js
+++ src/pages/Main/ProductsInMain.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ProductList from '../../components/ProductList/ProductList';
+import ProductList from '../ProductList/ProductList';
 import './productsinmain.scss';
 
 const ProductsInMain = () => {
